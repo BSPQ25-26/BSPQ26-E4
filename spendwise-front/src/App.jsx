9fc8d99800e6a0b5caf70fc9fc09aa9deb62c989
DiegoLopezAroca <diego.lopez.a@opendeusto.es>
2026-03-12 11:57:39 +0100

    hjf

--- spendwise-front/src/App.jsx
+++ spendwise-front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='red'>Vite + React</h1>
+      <h1 className="text-red-500">Vitesdf + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
