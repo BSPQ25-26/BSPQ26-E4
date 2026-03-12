48703667ce65f778dfa22761b210d1ae4f29c010
DiegoLopezAroca <diego.lopez.a@opendeusto.es>
2026-03-12 12:13:14 +0100

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+RUN BACKEND
+``cd spendwise-backend``
+``uvicorn app.main:app --reload``
+
+localhost:8080
+
+RUN FRONTEND:
+``cd spendwise-front``
+    if necessary:
+    ``npm install`` or ``npm i``
+    run project:
+    ``npm run dev``
--- spendwise-backend/app/main.py
+++ spendwise-backend/app/main.py
@@ -33,11 +33,3 @@ app.include_router(alerts_router,     prefix="/api/v1/alerts",     tags=["alerts
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
-
-
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
